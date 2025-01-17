a866aafc8ca85528c2f1129a5fb3d9d74fa3411a
Hitallo Azevedo <azevedohitallo@gmail.com>
2025-01-17 00:10:08 -0300

    feat: create header component and navigation menu

---
+++ src/app/components/ui/Header.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import Image from "next/image";
+import menuIcon from "@/../public/menu-icon.svg";
+import closeMenuIcon from "@/../public/close-icon.svg";
+import { useState } from "react";
+import "./style.css";
+
+interface IMenuButton {
+  isActive: boolean;
+  openIcon: string;
+  closeIcon: string;
+}
+
+function MenuButton({ isActive, openIcon, closeIcon }: IMenuButton) {
+  return !isActive ? (
+    <Image className="w-100" src={openIcon} alt="Open menu icon"></Image>
+  ) : (
+    <Image className="w-100" src={closeIcon} alt="Close menu icon"></Image>
+  );
+}
+
+export default function Header() {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  return (
+    <div>
+      <div className="app-header fixed w-screen backdrop-blur flex justify-between items-center border-b border-white p-3">
+        <div className="header-logo">
+          <h3 className="text-[1.5rem] text-white">
+            hitallo-<span className="text-[#00f500]">dev</span>
+          </h3>
+        </div>
+
+        <div
+          className={`menu-icon ${isMenuActive ? "w-[25px]": "w-[30px]"} mr-4`}
+          onClick={() => setIsMenuActive(!isMenuActive)}
+        >
+          <MenuButton
+            isActive={isMenuActive}
+            openIcon={menuIcon}
+            closeIcon={closeMenuIcon}
+          ></MenuButton>
+        </div>
+      </div>
+
+      <nav
+        className={`menuDrawer ${
+          isMenuActive ? "activeMenuDrawer" : ""
+        } absolute backdrop-blur w-screen top-16 p-5`}
+      >
+        <ul
+          className={`menuOptions ${
+            isMenuActive ? "activeMenuOptions" : ""
+          } flex flex-col gap-2 text-white items-center`}
+        >
+          <a href="#home">
+            <li>Home</li>
+          </a>
+          <a href="#about">
+            <li>About</li>
+          </a>
+          <a href="#projects">
+            <li>Projects</li>
+          </a>
+          <a href="#skills">
+            <li>Skills</li>
+          </a>
+          <a href="#contact">
+            <li>Contact</li>
+          </a>
+        </ul>
+      </nav>
+    </div>
+  );
+}
