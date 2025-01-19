84d22671bf23540a1f725ecd2ea94191e8445db0
Hitallo Azevedo <azevedohitallo@gmail.com>
2025-01-19 20:49:13 -0300

    feat: rendering skills and projects dynamically

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,35 @@
+"use client"
+
 import Header from "./components/ui/Header";
 import Home from "./components/sections/Home";
 import About from "./components/sections/About";
 import Skills from "./components/sections/Skills";
 import Projects from "./components/sections/Projects";
+import { useEffect, useState } from "react";
+import { getProjects } from "./api/getProjects";
+import { getSkills } from "./api/getSkills";
+import { IProject } from "./interfaces/Project";
+import { ISkill } from "./interfaces/Skill";
 
 export default function Page() {
+
+  const [projects, setProjects] = useState(Array<IProject>)
+  const [skills, setSkills] = useState(Array<ISkill>)
+
+  useEffect(() => {
+    (async () => {
+      setProjects( await getProjects() );
+      setSkills( await getSkills() );
+    })()
+  }, [])
+
   return (
     <div className="bg-zinc-800">
       <Header></Header>
       <Home></Home>
       <About></About>
-      <Skills></Skills>
-      <Projects></Projects>
+      <Skills skills={skills} ></Skills>
+      <Projects projects={projects} ></Projects>
     </div>
   );
 }
