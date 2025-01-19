4360d698bc43a77bc1d1835ce5367b4290b91b23
Hitallo Azevedo <azevedohitallo@gmail.com>
2025-01-19 16:49:56 -0300

    feat: rendering skills dynamically

--- src/app/components/sections/Skills/index.tsx
+++ src/app/components/sections/Skills/index.tsx
@@ -1,6 +1,27 @@
-export default function Skills(){
+import SkillCard from "../../ui/SkillCard"
+
+interface ISkill {
+    uuid: string;
+    title: string;
+    description: string;
+    svg_image: string;
+}
+
+export default async function Skills(){
+    
+    const response = await fetch("https://projects-repository.onrender.com/skills")
+    const data = await response.json()
+    const skills = data.data;
+
     return (
-        <>
-        </>
+        <section id="skills">
+            <div className="skills-header text-white py-20 mt-6">
+                <h2 className="text-4xl text-center font-semibold">What I know?</h2>
+            </div>
+            
+            <div className="skills-content flex flex-col items-center gap-5">
+                { skills.map((skill: ISkill) => <SkillCard imageUrl={skill.svg_image} title={skill.title} description={skill.description} key={skill.uuid}/>) }
+            </div>
+        </section>
     )
 }
