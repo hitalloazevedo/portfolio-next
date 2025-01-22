93e016304c3e1a58a09b7e2b74e47d2afa01e14a
Hitallo Azevedo <azevedohitallo@gmail.com>
2025-01-22 01:11:05 -0300

    feat: create Contact component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Home from "./components/sections/Home";
 import About from "./components/sections/About";
 import Skills from "./components/sections/Skills";
 import Projects from "./components/sections/Projects";
+import Contact from "./components/sections/Contact";
 import { useEffect, useState } from "react";
 import { getProjects } from "./api/getProjects";
 import { getSkills } from "./api/getSkills";
@@ -30,6 +31,7 @@ export default function Page() {
       <About></About>
       <Skills list={skills} ></Skills>
       <Projects list={projects} ></Projects>
+      <Contact></Contact>
     </div>
   );
 }
