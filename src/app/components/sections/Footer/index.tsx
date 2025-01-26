c1cd22792223e6b07b66af0541d3eae9f487a092
hitalloazevedo <azevedohitallo@gmail.com>
2025-01-26 14:14:58 -0300

    feat: general design adjusts

--- src/app/components/sections/Footer/index.tsx
+++ src/app/components/sections/Footer/index.tsx
@@ -2,13 +2,13 @@ import SocialLink from "../../ui/SocialLink"
 
 export default function Footer(){
     return (
-        <footer className="flex flex-col gap-8 mt-16">
+        <footer className="flex flex-col gap-12 mt-16">
             <div className="flex justify-center">
-                <a href="#home" className="flex gap-2 text-white hover:text-zinc-400">Launch to the top <p className="animate-floating-animation">🚀</p></a>
+                <a href="#home" className="flex flex-col-reverse items-center gap-1 text-white hover:text-zinc-400 duration-300 box-border">Launch to the top <p className="animate-floating-animation">🚀</p></a>
             </div>
             <div className="text-white px-4">
-                <h2 className="text-2xl font-semibold">hitallo-<span className="text-[#00f500]">dev</span></h2>
-                <p className="text-base text-zinc-400 mt-1">Driven by curiosity, fueled by code, focused on impact.</p>
+                <h2 className="text-xl font-semibold">hitallo-<span className="text-[#00f500]">dev</span></h2> 
+                <p className="text-sm text-zinc-400 mt-1">Driven by curiosity, fueled by code, focused on impact.</p>
             </div>
             <SocialLink/>
             <div className="bg-zinc-900 p-3">
