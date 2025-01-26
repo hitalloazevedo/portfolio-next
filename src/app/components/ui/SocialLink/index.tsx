45f655a23b470297b3ab35f6a6cd24920544e4a4
hitalloazevedo <azevedohitallo@gmail.com>
2025-01-26 14:15:55 -0300

    feat: add social media links to the anchors

--- src/app/components/ui/SocialLink/index.tsx
+++ src/app/components/ui/SocialLink/index.tsx
@@ -6,9 +6,9 @@ import instagramIcon from "@/../../public/instagram-icon.svg"
 export default function SocialLink(){
     return (
         <div className="social-media-container flex gap-5 items-center justify-center">
-        <a href=""><Image className="w-[22px] hover:scale-110 duration-100" src={githubIcon} alt=""></Image></a>
-        <a href=""><Image className="w-[30px] hover:scale-110 duration-100" src={linkedinIcon} alt=""></Image></a>
-        <a href=""><Image className="w-[28px] hover:scale-110 duration-100" src={instagramIcon} alt=""></Image></a>
+        <a target="_blank" href="https://github.com/hitalloazevedo"><Image className="w-[22px] hover:scale-110 duration-100" src={githubIcon} alt=""></Image></a>
+        <a target="_blank" href="https://www.linkedin.com/in/hitalloazevedo/"><Image className="w-[30px] hover:scale-110 duration-100" src={linkedinIcon} alt=""></Image></a>
+        <a target="_blank" href="https://www.instagram.com/hitalloo__"><Image className="w-[28px] hover:scale-110 duration-100" src={instagramIcon} alt=""></Image></a>
     </div>  
     )
 }
