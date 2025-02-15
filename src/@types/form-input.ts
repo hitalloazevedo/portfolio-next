fe3fad6c679a170c0904ae02d70ebf53c23edc18
hitalloazevedo <azevedohitallo@gmail.com>
2025-02-14 23:53:57 -0300

    sytle: removing blank space

--- src/@types/form-input.ts
+++ src/@types/form-input.ts
@@ -4,4 +4,4 @@ export const formInputSchema = z.object({
     name: z.string().min(5),
     email: z.string().regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
     message: z.string().min(5),
-  });
+});
